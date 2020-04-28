8402177a1ddf453da8f3962578616f43b61653bc
abbteaede <54662988+abbteaede@users.noreply.github.com>
2020-04-28 15:06:10 +0200

    nya koden

--- Projektbil.ino
+++ Projektbil.ino
@@ -1,12 +1,9 @@
 #include "EspMQTTClient.h"
-//#include "Servo.h"
-//#include "PubSubClient.h"
+#include "Servo.h"
 
-//void onConnectionEstablished();
-
-EspMQTTClient client(
-  "HK2",           // Wifi ssid
-  "Teaowilmer07",           // Wifi password
+EspMQTTClient client( //Inställningar för mqtt
+  "HK2",           // Wifi ssid HK2
+  "Teaowilmer07",           // Wifi password Teaowilmer07
   "maqiatto.com",  // MQTT broker ip
   1883,             // MQTT broker port
   "tea.edelborg@abbindustrigymnasium.se",            // MQTT username
@@ -17,13 +14,14 @@ EspMQTTClient client(
   true              // Enable debug messages
 );
 
-//Servo mittservo; //anger namnet på servo
+Servo mittservo; //anger namnet på servo
 #define motorPinRightDir 0 //D2
 #define motorPinRightSpeed 5//D1
 
 #define motorPinLeftDir 2
 #define motorPinLeftSpeed 4
 
+int deg = 0; //definerar värden som kommer användas
 int fart = 0;
 bool go = false;
   
@@ -34,60 +32,53 @@ void setup() {
   pinMode(motorPinRightSpeed, OUTPUT);
   pinMode(motorPinLeftDir, OUTPUT);
   pinMode(motorPinLeftSpeed, OUTPUT);
-  //mittservo.attach(14);
-  //mittservo.write(0);
+  mittservo.attach(15); 
+  mittservo.write(90);
 }
-void onConnectionEstablished()
+void onConnectionEstablished() //funktion hämtar värdena från mqtt och gör om värdena för motorn och servot
 {
-  client.subscribe("tea.edelborg@abbindustrigymnasium.se/speed", [](const String &payload) {
-    fart = payload.toInt();
-    if (go==true) {
+  client.subscribe("tea.edelborg@abbindustrigymnasium.se/speed", [](const String &payload) { //hämtar från topicen speed
+    fart = payload.toInt(); //gör om stringen till en int
+    if (go==true) { //körs bara om bilen har satts på
     Serial.println(fart);
-    digitalWrite(motorPinRightSpeed, fart);
+    digitalWrite(motorPinRightSpeed, fart); //sätter farten till båda hjulen
     digitalWrite(motorPinLeftSpeed, fart);
     }
   });
-  client.subscribe("tea.edelborg@abbindustrigymnasium.se/backforward", [](const String &payload) {
+  client.subscribe("tea.edelborg@abbindustrigymnasium.se/backforward", [](const String &payload) { //hämtar värdet från backforward topicen
+    Serial.println(payload);
     if (go == true) {
-    if (payload == "framåt")
-    {
-      digitalWrite(motorPinRightDir, 0);
-      digitalWrite(motorPinLeftDir, 0);
-    }
-    else
+    if (payload == "1") //Om payload är 1 kör bilen bakåt
     {
       digitalWrite(motorPinRightDir, 1);
       digitalWrite(motorPinLeftDir, 1);
     }
+    else if (payload == "0") //annars kör den framåt
+    {
+      digitalWrite(motorPinRightDir, 0);
+      digitalWrite(motorPinLeftDir, 0);
+    }
     }
   });
-  client.subscribe("tea.edelborg@abbindustrigymnasium.se/drive", [](const String &payload) {
+  client.subscribe("tea.edelborg@abbindustrigymnasium.se/drive", [](const String &payload) { //om bilen är på eller inte, beroende om sidan är på eller inte
     Serial.println(payload);
-    if (payload == "1")
+    if (payload == "1") //om den är på kan de andra värdena ändras
     {
       go = true;
     }
-    else if (payload == "0")
+    else if (payload == "0") //om det är false sätts farten till 0
     {
       go = false;
       digitalWrite(motorPinRightSpeed, 0);
       digitalWrite(motorPinLeftSpeed, 0);
     }
   });
+  client.subscribe("tea.edelborg@abbindustrigymnasium.se/deg", [](const String &payload) { //hämtar vinkeln till servot
+    deg= payload.toInt(); //gör om det till en int
+    mittservo.write(deg); //sätter servot till det värdet
+  });
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-  client.loop();
+  client.loop(); //kör setup om och om igen
 
 }
-
-//connecta till MQTT
-//includa libraries
-//defina hjulen
-//defina allting i void setup
-//gör en funktion där du kopplar upp dig till MQTT och lägg allt annat i den
-//fixa en av och på 
-//Om det är på hämta senaste värdet från MQTT och kö så snabbt, även ta om det är bakåt eller framåt
-//Gör en funktion för att svänga, ta då om talet är större än 90 grader sväng höger, om talet är mindre än 90 grader sväng vänster
-//När talet är en av dem så sätter man degress till ett visst värde och inte tex 10000, görs med en if sats
-//Lampa som blinkar när man backar
